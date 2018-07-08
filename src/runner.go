43520eb830a15a8de6f2d1db3bb4a21f72b3c9b1
hiscaler <hiscaler@gmail.com>
2018-07-08 23:51:00 +0800

    完善关键词接口解析

--- src/runner.go
+++ src/runner.go
@@ -1,17 +1,38 @@
 package main
 
 import (
-	"site"
 	"fmt"
+	"net/http"
+	"url"
+	"errors"
+	"io/ioutil"
+	"encoding/json"
+	"api"
+	"site"
 )
 
 func main() {
-	seedKeywords := []string{"Bags"}
-	g := new(site.Google)
-	g.Keywords = make(map[string]site.Keyword)
-	for _, seedKeyword := range seedKeywords {
-		g.SetSeed(seedKeyword)
-		g.Search()
-		fmt.Println(*g)
+	keywordIndexResponse := api.KeywordIndexResponse{}
+	resp, err := http.Get(url.KeywordIndex())
+	if err != nil {
+		errors.New(err.Error())
+	}
+	if resp.StatusCode == 200 {
+		fmt.Println(resp.Body)
+		body, _ := ioutil.ReadAll(resp.Body)
+		if err := json.Unmarshal([]byte(body), &keywordIndexResponse); err == nil {
+			g := new(site.Google)
+			g.Keywords = make(map[string]site.Keyword)
+			for _, item := range keywordIndexResponse.Data.Items {
+				fmt.Printf("Site Name = %s, Id = %d, Name = %s", item.SiteName, item.Id, item.Name)
+				g.SetSeed(item.Name)
+				g.Search()
+				fmt.Println(*g)
+			}
+		} else {
+			fmt.Println(err)
+		}
+	} else {
+		errors.New("Can't get keywords list.")
 	}
 }
