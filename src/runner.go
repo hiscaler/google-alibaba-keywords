c48ca4152ebb021ef61960dcc8cd43606c2bf1ae
hiscaler <hiscaler@gmail.com>
2018-07-09 23:19:35 +0800

    添加日志功能

--- src/runner.go
+++ src/runner.go
@@ -9,25 +9,25 @@ import (
 	"site"
 	"response"
 	"github.com/kr/pretty"
+	"logger"
 )
 
 func main() {
 	keywordIndexResponse := response.KeywordIndex{}
 	resp, err := http.Get(url.KeywordIndex())
 	if err != nil {
-		fmt.Println(err)
+		logger.Instance.Error(err.Error())
 	} else {
 		if resp.StatusCode == 200 {
-			fmt.Println(resp.Body)
 			body, _ := ioutil.ReadAll(resp.Body)
 			if err := json.Unmarshal([]byte(body), &keywordIndexResponse); err == nil {
 				g := new(site.Google)
 				g.Keywords = make(map[string]site.Keyword)
 				for _, item := range keywordIndexResponse.Data.Items {
-					fmt.Printf("Site Name = %s, Id = %d, Name = %s\r\n", item.SiteName, item.Id, item.Name)
+					logger.Instance.Info(fmt.Sprintf("Site Name = %s, Id = %d, Name = %s", item.SiteName, item.Id, item.Name))
 					g.SetSeed(item.Name)
 					g.Search()
-					fmt.Println(pretty.Formatter(*g))
+					logger.Instance.Debug(fmt.Sprintf("%# v", pretty.Formatter(*g)))
 				}
 			} else {
 				fmt.Println(err)
@@ -37,4 +37,5 @@ func main() {
 		}
 	}
 
+	defer logger.Instance.Close()
 }
--- src/site/google.go
+++ src/site/google.go
@@ -9,7 +9,7 @@ import (
 	"time"
 	"github.com/PuerkitoBio/goquery"
 	"strings"
-	"log"
+	"logger"
 )
 
 type Google struct {
@@ -44,7 +44,7 @@ func (g *Google) SetSeed(seedKeyword string) *Google {
 }
 
 func (g *Google) Search() *Google {
-	println(g.getUrl())
+	logger.Instance.Info(g.getUrl())
 	request, _ := http.NewRequest("GET", g.getUrl(), nil)
 	request.Header.Set("Connection", "keep-alive")
 	request.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
@@ -62,14 +62,14 @@ func (g *Google) Search() *Google {
 		if response.StatusCode == 200 {
 			content, err := ioutil.ReadAll(response.Body)
 			if err != nil {
-				fmt.Printf("Read response body error: %s", err)
+				logger.Instance.Debug(fmt.Sprintf("Read response body error: %s", err))
 			} else {
 				doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(content)))
 				if err == nil {
 					doc.Find("#brs .card-section a").Each(func(i int, selection *goquery.Selection) {
 						name := strings.Trim(selection.Text(), " ")
 						url, _ := selection.Attr("href")
-						fmt.Printf("#%02d Keyword name = '%s' url = %s\n", i+1, name, url)
+						logger.Instance.Info(fmt.Sprintf("#%02d Keyword name = '%s' url = %s", i+1, name, url))
 						keyword := new(Keyword)
 						if g.Level == 0 {
 							keyword.Class = directoryKeyword
@@ -83,13 +83,13 @@ func (g *Google) Search() *Google {
 					})
 					g.Level += 1
 				} else {
-					log.Fatal(err)
+					logger.Instance.Info(err.Error())
 				}
 			}
 		}
 		defer response.Body.Close()
 	} else {
-		fmt.Printf("HTTP error: %s", err)
+		logger.Instance.Error(fmt.Sprintf("HTTP error: %s", err))
 	}
 
 	return g
