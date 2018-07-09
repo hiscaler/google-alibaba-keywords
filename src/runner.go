9f583f1aa16d1fb1704cb516b6ff8e4908d9a9ac
hiscaler <hiscaler@gmail.com>
2018-07-09 17:22:16 +0800

    ...

--- src/runner.go
+++ src/runner.go
@@ -4,35 +4,37 @@ import (
 	"fmt"
 	"net/http"
 	"url"
-	"errors"
 	"io/ioutil"
 	"encoding/json"
 	"site"
 	"response"
+	"github.com/kr/pretty"
 )
 
 func main() {
 	keywordIndexResponse := response.KeywordIndex{}
 	resp, err := http.Get(url.KeywordIndex())
 	if err != nil {
-		errors.New(err.Error())
-	}
-	if resp.StatusCode == 200 {
-		fmt.Println(resp.Body)
-		body, _ := ioutil.ReadAll(resp.Body)
-		if err := json.Unmarshal([]byte(body), &keywordIndexResponse); err == nil {
-			g := new(site.Google)
-			g.Keywords = make(map[string]site.Keyword)
-			for _, item := range keywordIndexResponse.Data.Items {
-				fmt.Printf("Site Name = %s, Id = %d, Name = %s", item.SiteName, item.Id, item.Name)
-				g.SetSeed(item.Name)
-				g.Search()
-				fmt.Println(*g)
+		fmt.Println(err)
+	} else {
+		if resp.StatusCode == 200 {
+			fmt.Println(resp.Body)
+			body, _ := ioutil.ReadAll(resp.Body)
+			if err := json.Unmarshal([]byte(body), &keywordIndexResponse); err == nil {
+				g := new(site.Google)
+				g.Keywords = make(map[string]site.Keyword)
+				for _, item := range keywordIndexResponse.Data.Items {
+					fmt.Printf("Site Name = %s, Id = %d, Name = %s\r\n", item.SiteName, item.Id, item.Name)
+					g.SetSeed(item.Name)
+					g.Search()
+					fmt.Println(pretty.Formatter(*g))
+				}
+			} else {
+				fmt.Println(err)
 			}
 		} else {
-			fmt.Println(err)
+			fmt.Println("Can't get keywords list.")
 		}
-	} else {
-		errors.New("Can't get keywords list.")
 	}
+
 }
